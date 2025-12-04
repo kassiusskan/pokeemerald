2140dc191c28bceb4c052a266ab7dc58c383faec
kassiusskan <102862300+kassiusskan@users.noreply.github.com>
2025-12-03 23:59:33 -0300

    Modificaçoes visuais em algumas rotas

--- include/config/overworld.h
+++ include/config/overworld.h
@@ -95,7 +95,7 @@
 #define OW_TIME_OF_DAY_FALLBACK         TIME_MORNING // The time of day that encounter tables fall back to.
 
 // Lighting
-#define OW_SHADOW_INTENSITY             6       // Ranges from 0 to 16, where 0 is fully transparent and 16 is black.
+#define OW_SHADOW_INTENSITY             4       // Ranges from 0 to 16, where 0 is fully transparent and 16 is black.
 #define OW_OBJECT_SUBPRIORITY           148     // The higher the value, the farther back compared to other sprites. Shadows should be behind object events.
 #define OW_ENABLE_DNS                   TRUE    // If set to TRUE, the overworld will be tinted depending on time of day.
 
