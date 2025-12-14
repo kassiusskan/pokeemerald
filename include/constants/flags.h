ccb0af8d015a2ca3fb350da864f456215bfb9369
kassiusskan <102862300+kassiusskan@users.noreply.github.com>
2025-12-14 14:19:30 -0300

    Config inicial  Speaker Name

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -461,7 +461,7 @@
 #define FLAG_REGISTERED_WALLACE              (TRAINER_REGISTERED_FLAGS_START + REMATCH_WALLACE)
 
 #define FLAG_DYNAMAX_BATTLE                    0x1AA // Unused Flag
-#define FLAG_TERA_ORB_CHARGED                     0x1AB // Unused Flag
+#define FLAG_TERA_ORB_CHARGED                  0x1AB // Unused Flag
 
 #define FLAG_DEFEATED_DEOXYS                 0x1AC
 #define FLAG_BATTLED_DEOXYS                  0x1AD
