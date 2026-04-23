b1711e62ac428b6a564f75ce7c22978546f4259a
kassiusskan <102862300+kassiusskan@users.noreply.github.com>
2026-04-23 08:52:24 -0300

    Revert "Pre Pull update"
    This reverts commit 4493daef0ed0b9274cda7f209a96c99090796197.

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -656,10 +656,10 @@
 #define FLAG_HIDDEN_ITEM_ROUTE_123_RARE_CANDY                (FLAG_HIDDEN_ITEMS_START + 0x6E)
 #define FLAG_HIDDEN_ITEM_ROUTE_105_BIG_PEARL                 (FLAG_HIDDEN_ITEMS_START + 0x6F)
 
-#define FLAG_EXP_SHARE                                        0x264 // Unused Flag
-#define FLAG_SUMMARY_SCREEN_IV_EV_INFO                        0x265 // Unused Flag
-#define FLAG_HIDDEN_ITEM_BOTTLE_CAP                           0x266 // Unused Flag
-#define FLAG_HIDDEN_ITEM_GOLD_BOTTLE_CAP                      0x267 // Unused Flag
+#define FLAG_EXP_SHARE  0x264 // Unused Flag
+#define FLAG_SUMMARY_SCREEN_IV_EV_INFO  0x265 // Unused Flag
+#define FLAG_UNUSED_0x266  0x266 // Unused Flag
+#define FLAG_UNUSED_0x267  0x267 // Unused Flag
 #define FLAG_UNUSED_0x268  0x268 // Unused Flag
 #define FLAG_UNUSED_0x269  0x269 // Unused Flag
 #define FLAG_UNUSED_0x26A  0x26A // Unused Flag
